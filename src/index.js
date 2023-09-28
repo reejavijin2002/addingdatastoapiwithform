4e51392b5694e7281446b72ffe5610ef87c7d8ca
reejavijin2002 <vijincp7@gmail.com>
2023-09-28 10:28:01 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  Form from './Form';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Form/> */}
+    {
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Form/>}/>
+        <Route path='/App' element={<App/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    }
   </React.StrictMode>
 );
 
